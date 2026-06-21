c6c0acaec2aa3ed5f797a8bdf5b3dbe565cd7c08
Jotalea <main@jotalea.com.ar>
2026-06-21 15:58:46 -0300

    new wallpaper

--- .jotalea/jotawm.sh
+++ .jotalea/jotawm.sh
@@ -3,6 +3,6 @@
 killall -q polybar picom
 while pgrep -u $USER -x polybar >/dev/null; do sleep 1; done
 
-feh --bg-fill ~/.jotalea/wallpapers/flowering-rain.png &
+feh --bg-fill ~/.jotalea/assets/flowering-rain.png &
 picom --config ~/.config/picom/picom.conf &
 polybar -config ~/.config/polybar/config.ini &
